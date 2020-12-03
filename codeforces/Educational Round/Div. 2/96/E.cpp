cf992a7d05559afcadcbf59392522c0530649895
lugang <lugang@bytedance.com>
2020-12-03 11:52:05 +0800

    MOD: index page

--- codeforces/Educational Round/Div. 2/96/E.cpp
+++ codeforces/Educational Round/Div. 2/96/E.cpp
@@ -1,19 +1,36 @@
 #include <iostream>
-
+#include <algorithm>
+#include <vector>
 using namespace std;
 
-long long cal(string& s, int l, int r) {
-    if(l >= r) return 0;
-    if(s[l]==s[r]) return cal(s, l+1, r-1);
-    if(s[l+1]==s[r] || s[l]==s[r-1]) {
-        return r-l + cal(s, l+1, r-1);
-    }
-    return 2*(r-l)-1+cal(s, l+1, r-1);
+const long long maxn = 200001;
+long long seg[maxn];
+
+void update(long long n, long long x, long long val) {
+    while(x <= n) {seg[x] += val; x += x&(-x);}
+}
+
+long long query(long long x) {
+    long long res = 0;
+    while(x>0) {res += seg[x]; x -= x&(-x);}
+    return res;
 }
 
 int main(){
-    int n; string s;
+    long long n; string s;
     cin>>n>>s;
-    cout<<cal(s, 0, n-1)<<endl;
+    string t = s;
+    reverse(t.begin(), t.end());
+    vector<vector<long long>> vec(26);
+    vector<long long> idx(26, 0);
+    for(long long i=0;i<n;i++) vec[s[i]-'a'].push_back(i);
+    for(long long i=0;i<n;i++) update(n, i+1, 1);
+    long long res = 0;
+    for(char c: t) {
+        long long j = vec[c-'a'][idx[c-'a']++];
+        res += query(j);
+        update(n, j+1, -1);
+    }
+    cout<<res<<endl;
     return 0;
 }
