6fab880a75d15dd1676be6c10196606e50147181
福尔摩斯卢 <fuermosilu@fuermosiludeMacBook-Pro.local>
2020-12-08 09:52:45 +0800

    ADD: codeforces/Educational Round/Div. 2/99

---
+++ codeforces/Educational Round/Div. 2/99/C.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    long t, x, y;
+    cin>>t;
+    while(t--) {
+        cin>>x>>y;
+        cout<<(x-1)<<" "<<y<<endl;
+    }
+    return 0;
+}
